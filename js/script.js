b9ad07d543878b7429d40e663961852c6539f636
Candelaria <ferrari.candelaria@gmail.com>
2026-05-20 09:48:16 -0300

    feat: Funcion de generar paleta de color aleatoria

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 /* sidebar */
-const wrapper   = document.getElementById('wrapper');
+const wrapper = document.getElementById('wrapper');
 const btnToggle = document.getElementById('btnToggle');
 
 /* Seleccionar tamaño de paletas */
@@ -11,6 +11,7 @@ radios.forEach(radio => {
       '--palette-size',
       radio.value  // "6", "8" o "9"
     );
+    generarPaleta(); // ← esto es todo lo que faltaba
   });
 });
 
@@ -33,3 +34,47 @@ function toggleSidebar() {
   );
 }
 btnToggle.addEventListener('click', toggleSidebar);
+
+/* Generar un color HEX aleatorio */
+function randomHex() { /* crea un color aleatoreamente  */
+  return '#' + Math.floor(Math.random() * 16777215) /* 16777215 es el número decimal de #ffffff */
+    .toString(16) /* Math.random() genera un número entre 0 y 1, lo multiplicás por ese máximo y convertís a base 16. */
+    .padStart(6, '0');
+}
+/* Generar una paleta completa */
+function generarPaleta() {
+  // Lee cuántos colores eligió el usuario
+  const cantidad = document.querySelector('input[name="tamanio"]:checked').value;
+
+  const grid = document.getElementById('paletteGrid');
+  grid.innerHTML = ''; // limpia las cards anteriores
+
+  for (let i = 0; i < cantidad; i++) {
+    const color = randomHex();
+
+    const card = document.createElement('div');
+    card.className = 'card-color';
+    card.innerHTML = `
+      <div class="color" style="background-color: ${color}"></div>
+      <span class="color-info">
+        <p class="color-code">${color}</p>
+        <span class="copy-icon">⧉</span>
+      </span>
+    `;
+
+    grid.appendChild(card);
+  }
+}
+/* Conectarlo al botón y al teclado */
+document.getElementById('generateBtn').addEventListener('click', generarPaleta);
+
+document.addEventListener('keydown', (e) => {
+  if (e.code === 'Space' && e.target === document.body) {
+    e.preventDefault();
+    generarPaleta();
+  }
+});
+/* Cargar una primera paleta apenas carga el html */
+document.addEventListener('DOMContentLoaded', () => { /* DOMContentLoaded se dispara cuando el HTML terminó de cargar */
+  generarPaleta();
+});
